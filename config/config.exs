5725c6f03e50709ef888f375339fc06b4244cf45
yui15 <fumufumu1510@gmail.com>
2024-01-09 17:33:47 +0900

    ch12

--- config/config.exs
+++ config/config.exs
@@ -36,6 +36,9 @@ config :nano_planner, NanoPlanner.Repo,
 # Configures the default time zone
 config :nano_planner, default_time_zone: "Asia/Tokyo"
 
+# Configures the default time locale
+config :nano_planner, NanoPlannerWeb.Gettext, default_locale: "ja"
+
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
